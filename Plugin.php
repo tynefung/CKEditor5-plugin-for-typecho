fcb1860377a9341022d4d5ce632bb50fd57c2adc
Song Feng <fengsong@whu.edu.cn>
2024-08-08 23:02:47 +0800

    0.0.2

---
+++ Plugin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * CKEditor5 for Typecho
+ *
+ * @package CKEditor5
+ * @author tynefung
+ * @version 0.0.2
+ * @link https://github.com/tynefung
+ */
+class CKEditor5_Plugin implements Typecho_Plugin_Interface
+{
+    public static function activate() {
+        Typecho_Plugin::factory('admin/write-post.php')->richEditor = array('CKEditor5_Plugin', 'render');
+        Typecho_Plugin::factory('admin/write-page.php')->richEditor = array('CKEditor5_Plugin', 'render');
+    }
+
+    public static function deactivate(){}
+
+    public static function config(Typecho_Widget_Helper_Form $form){}
+    
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    public static function render() {
+        $ckEditor5 = Typecho_Common::url('CKEditor/ckeditor5', Helper::options()->pluginUrl);
+        echo <<<CODE
+                <link rel="stylesheet" href="{$ckEditor5}/ckeditor5.css" />
+                <script type="importmap">
+                {
+                    "imports": {
+                        "ckeditor5": "{$ckEditor5}/ckeditor5.js",
+                        "ckeditor5/": "{$ckEditor5}/"
+                    }
+                }
+                </script>
+                <script></script>
+                
+                <script type="module" src="{$ckEditor5}/init.js"></script>
+        CODE;
+    }
+}
---
+++ ckeditor5/init.js
@@ -0,0 +1,399 @@
+import translations from 'ckeditor5/translations/zh-cn.js';
+import {
+  ClassicEditor,
+  AccessibilityHelp,
+  Alignment,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  BalloonToolbar,
+  BlockQuote,
+  Bold,
+  Code,
+  CodeBlock,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  FullPage,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  HtmlComment,
+  HtmlEmbed,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListProperties,
+  Markdown,
+  MediaEmbed,
+  PageBreak,
+  Paragraph,
+  PasteFromMarkdownExperimental,
+  PasteFromOffice,
+  RemoveFormat,
+  SelectAll,
+  ShowBlocks,
+  SimpleUploadAdapter,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
+  Underline,
+  Undo
+} from 'ckeditor5';
+
+$(document).ready(() => {
+  
+  const editorConfig = {
+    toolbar: {
+      items: [
+        'undo',
+        'redo',
+        '|',
+        'sourceEditing',
+        'showBlocks',
+        '|',
+        'heading',
+        'style',
+        '|',
+        'fontSize',
+        'fontFamily',
+        'fontColor',
+        'fontBackgroundColor',
+        '|',
+        'bold',
+        'italic',
+        'underline',
+        '|',
+        'link',
+        'insertImage',
+        'insertTable',
+        'highlight',
+        'blockQuote',
+        'codeBlock',
+        '|',
+        'alignment',
+        '|',
+        'bulletedList',
+        'numberedList',
+        'todoList',
+        'outdent',
+        'indent'
+      ],
+      shouldNotGroupWhenFull: true
+    },
+    plugins: [
+      AccessibilityHelp,
+      Alignment,
+      Autoformat,
+      AutoImage,
+      AutoLink,
+      Autosave,
+      BalloonToolbar,
+      BlockQuote,
+      Bold,
+      Code,
+      CodeBlock,
+      Essentials,
+      FindAndReplace,
+      FontBackgroundColor,
+      FontColor,
+      FontFamily,
+      FontSize,
+      FullPage,
+      GeneralHtmlSupport,
+      Heading,
+      Highlight,
+      HorizontalLine,
+      HtmlComment,
+      HtmlEmbed,
+      ImageBlock,
+      ImageCaption,
+      ImageInline,
+      ImageInsertViaUrl,
+      ImageResize,
+      ImageStyle,
+      ImageTextAlternative,
+      ImageToolbar,
+      Indent,
+      IndentBlock,
+      Italic,
+      Link,
+      LinkImage,
+      List,
+      ListProperties,
+      Markdown,
+      MediaEmbed,
+      PageBreak,
+      Paragraph,
+      PasteFromMarkdownExperimental,
+      PasteFromOffice,
+      RemoveFormat,
+      SelectAll,
+      ShowBlocks,
+      SimpleUploadAdapter,
+      SourceEditing,
+      SpecialCharacters,
+      SpecialCharactersArrows,
+      SpecialCharactersCurrency,
+      SpecialCharactersEssentials,
+      SpecialCharactersLatin,
+      SpecialCharactersMathematical,
+      SpecialCharactersText,
+      Strikethrough,
+      Style,
+      Subscript,
+      Superscript,
+      Table,
+      TableCaption,
+      TableCellProperties,
+      TableColumnResize,
+      TableProperties,
+      TableToolbar,
+      TextTransformation,
+      TodoList,
+      Underline,
+      Undo
+    ],
+    balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImageViaUrl', '|', 'bulletedList', 'numberedList'],
+    fontFamily: {
+      supportAllValues: true
+    },
+    fontSize: {
+      options: [10, 12, 14, 'default', 18, 20, 22],
+      supportAllValues: true
+    },
+    heading: {
+      options: [
+        {
+          model: 'paragraph',
+          title: 'Paragraph',
+          class: 'ck-heading_paragraph'
+        },
+        {
+          model: 'heading1',
+          view: 'h1',
+          title: 'Heading 1',
+          class: 'ck-heading_heading1'
+        },
+        {
+          model: 'heading2',
+          view: 'h2',
+          title: 'Heading 2',
+          class: 'ck-heading_heading2'
+        },
+        {
+          model: 'heading3',
+          view: 'h3',
+          title: 'Heading 3',
+          class: 'ck-heading_heading3'
+        },
+        {
+          model: 'heading4',
+          view: 'h4',
+          title: 'Heading 4',
+          class: 'ck-heading_heading4'
+        },
+        {
+          model: 'heading5',
+          view: 'h5',
+          title: 'Heading 5',
+          class: 'ck-heading_heading5'
+        },
+        {
+          model: 'heading6',
+          view: 'h6',
+          title: 'Heading 6',
+          class: 'ck-heading_heading6'
+        }
+      ]
+    },
+    htmlSupport: {
+      allow: [
+        {
+          name: /^.*$/,
+          styles: true,
+          attributes: true,
+          classes: true
+        }
+      ]
+    },
+    image: {
+      toolbar: [
+        'toggleImageCaption',
+        'imageTextAlternative',
+        '|',
+        'imageStyle:inline',
+        'imageStyle:wrapText',
+        'imageStyle:breakText',
+        '|',
+        'resizeImage'
+      ]
+    },
+    language: 'zh-cn',
+    link: {
+      addTargetToExternalLinks: true,
+      defaultProtocol: 'https://',
+      decorators: {
+        toggleDownloadable: {
+          mode: 'manual',
+          label: 'Downloadable',
+          attributes: {
+            download: 'file'
+          }
+        }
+      }
+    },
+    list: {
+      properties: {
+        styles: true,
+        startIndex: true,
+        reversed: true
+      }
+    },
+    menuBar: {
+      isVisible: true
+    },
+    placeholder: 'Type or paste your content here!',
+    style: {
+      definitions: [
+        {
+          name: 'Article category',
+          element: 'h3',
+          classes: ['category']
+        },
+        {
+          name: 'Title',
+          element: 'h2',
+          classes: ['document-title']
+        },
+        {
+          name: 'Subtitle',
+          element: 'h3',
+          classes: ['document-subtitle']
+        },
+        {
+          name: 'Info box',
+          element: 'p',
+          classes: ['info-box']
+        },
+        {
+          name: 'Side quote',
+          element: 'blockquote',
+          classes: ['side-quote']
+        },
+        {
+          name: 'Marker',
+          element: 'span',
+          classes: ['marker']
+        },
+        {
+          name: 'Spoiler',
+          element: 'span',
+          classes: ['spoiler']
+        },
+        {
+          name: 'Code (dark)',
+          element: 'pre',
+          classes: ['fancy-code', 'fancy-code-dark']
+        },
+        {
+          name: 'Code (bright)',
+          element: 'pre',
+          classes: ['fancy-code', 'fancy-code-bright']
+        }
+      ]
+    },
+    table: {
+      contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+    },
+    codeBlock: {
+      languages: [
+        { language: "plaintext", label: "Plain text" },
+        { language: "apache", label: "Apache" },
+        { language: "bash", label: "Bash" },
+        { language: "c", label: "C" },
+        { language: "cmake", label: "CMake" },
+        { language: "clojure", label: "Clojure" },
+        { language: "console", label: "Shell session" },
+        { language: "cs", label: "C#" },
+        { language: "cpp", label: "C++" },
+        { language: "css", label: "CSS" },
+        { language: "dart", label: "Dart" },
+        { language: "diff", label: "Diff" },
+        { language: "dockerfile", label: "Dockerfile" },
+        { language: "elisp", label: "Emacs Lisp" },
+        { language: "elixir", label: "Elixir" },
+        { language: "erlang", label: "Erlang" },
+        { language: "go", label: "Go" },
+        { language: "html", label: "HTML" },
+        { language: "java", label: "Java" },
+        { language: "javascript", label: "JavaScript" },
+        { language: "json", label: "JSON" },
+        { language: "json5", label: "JSON5" },
+        { language: "kotlin", label: "Kotlin" },
+        { language: "nginx", label: "Nginx" },
+        { language: "php", label: "PHP" },
+        { language: "python", label: "Python" },
+        { language: "ruby", label: "Ruby" },
+        { language: "scala", label: "Scala" },
+        { language: "typescript", label: "TypeScript" },
+        { language: "xml", label: "XML" },
+        { language: "yaml", label: "YAML" },
+      ],
+    },
+    translations: [translations]
+  };
+
+  ClassicEditor.create(document.querySelector("#text"), editorConfig)
+    .then((editor) => {
+      Typecho.insertFileToEditor = (file, url, isImage) => {
+        if (isImage) {
+          editor.execute("insertImage", {
+            source: [{ src: url, alt: file }],
+          });
+        } else {
+          editor.model.change((writer) => {
+            const insertPos =
+              editor.model.document.selection.getFirstPosition();
+            writer.insertText(file, { linkHref: url }, insertPos);
+          });
+        }
+      };
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
